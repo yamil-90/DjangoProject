8af00701f0fc15df8f284e69f0f976bf539bed01
yamil-90 <yamil.coloma90@gmail.com>
2023-03-29 12:28:40 -0300

    added choices to question model

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Poll
+from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Poll)
+admin.site.register(Question)
+admin.site.register(Choice)
--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-from .models import Poll
+from .models import Question
 
 def index(request):
-	polls = Poll.objects.all()
-	return render(request, 'polls/index.html', {'polls': polls})
+	questions = Question.objects.all()
+	return render(request, 'polls/index.html', {'questions': questions})
 
-def detail(request, poll_id):
+def detail(request, question_id):
     try:
-        poll = Poll.objects.get(id=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404("Poll does not exist :(")
+        question = Question.objects.get(id=question_id)
+    except question.DoesNotExist:
+        raise Http404("Question does not exist :(")
     
-    return render(request, 'polls/detail.html', {'poll': poll})
+    return render(request, 'questions/detail.html', {'question': question})
