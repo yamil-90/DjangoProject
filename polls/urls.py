12f1af0cd6ccb8da9f03b758b3e947a65a50f0ff
yamil-90 <yamil.coloma90@gmail.com>
2023-04-01 18:49:12 -0300

    added more views and urls

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
 	path('', views.index, name="index"),
-    path('<int:poll_id>/', views.detail, name="detail"),
+    path('owner', views.owner, name="owner"),
+    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:question_id>/results/', views.results, name="results"),
+    path('<int:question_id>/vote/', views.vote, name="vote")
+    
 ]
