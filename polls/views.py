eb2db23429bde893cde7254db5cc8c5ac3889992
yamil-90 <yamil.coloma90@gmail.com>
2023-04-07 00:01:01 -0300

    added missing redirect

--- polls/views.py
+++ polls/views.py
@@ -19,7 +19,7 @@ class DetailView(DetailView):
 class ResultsView(DetailView):
     model = Question
     template_name = 'polls/results.html'
-    
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -36,7 +36,7 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:detail', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 def owner(request):
     return HttpResponse("Hello, world. 05ede5ce is the polls index.")
