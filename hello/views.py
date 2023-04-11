54a2f5bc528f12e15d307e74b7ddb4034072cd30
yamil-90 <yamil.coloma90@gmail.com>
2023-04-11 16:16:13 -0300

    added view count

--- hello/views.py
+++ hello/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    resp = HttpResponse("Hello, world. You're at the hello index.")
-    resp.set_cookie('dj4e_cookie', '05ede5ce', max_age=1000)
-    return resp
+    print("session ", request.session['num_visits'])
+    count = request.session.get('num_visits', 0) + 1
+    request.session['num_visits'] = count
+    response = HttpResponse("Hello, world. You're at the hello index.<br>view count="+str(count))
+    response.set_cookie('dj4e_cookie', '05ede5ce', max_age=1000)
+    return response
