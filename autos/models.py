0385db8fea1a5c2ce917d417737343bbc7e6f068
yamil-90 <yamil.coloma90@gmail.com>
2023-04-17 21:09:28 -0300

    lol forgot the model name

--- autos/models.py
+++ autos/models.py
@@ -5,10 +5,16 @@ from django.db import models
 class Make(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Car(models.Model):
     nickname = models.CharField(max_length=200)
     make = models.ForeignKey(Make, on_delete=models.CASCADE)
     mileage = models.IntegerField(default=0)
     comments = models.TextField(max_length=500)
+
+    def __str__(self):
+            return self.name
 
     
